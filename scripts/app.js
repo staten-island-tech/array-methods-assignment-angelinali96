4ab9af266fcb796a19caab7f300f1674cf2d6880
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2023-10-05 10:15:39 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,178 +1,84 @@
 const tangerineNovels =  [
     {
-      cat : "original-GL-contemporary-mystery",
-      author : "酒暖春深",
-      bookName : "我亲爱的法医小姐",
-      detailUrl : "4170491",
-      wordCount : "5294309888",
-      status : "878082"
+      tags : ["original","GL","contemporary","mystery"],
+      author : `酒暖春深`,
+      name : "My Dear Miss Forensic",
+      wordCount : 5294309888,
+      complete : true
     },
     {
-      cat : "original-GL-contemporary-romance",
-      author : "扶华",
-      bookName : "她的山，她的海",
-      detailUrl : "4472959",
-      wordCount : "3377921280",
-      status : "227833"
+      tags : ["original","GL","contemporary","romance"],
+      author : `扶华`,
+      name : "Her Mountains, Her Seas",
+      wordCount : 3377921280,
+      complete : true
     },
     {
-      cat : "original-GL-contemporary-mystery",
-      author : "君sola",
-      bookName : "探虚陵现代篇",
-      detailUrl : "1473506",
-      wordCount : "10473548800",
-      status : "3019766"
+      tags : ["original","GL","contemporary","mystery"],
+      author : `君sola`,
+      name : "Tan Xu Ling [Contemporary]",
+      wordCount : 10473548800,
+      complete : false
     },
     {
-      cat : "original-GL-contemporary-romance",
-      author : "闵然",
-      bookName : "余生为期",
-      detailUrl : "2504000",
-      wordCount : "2188042752",
-      status : "769957"
+      tags : ["original","GL","contemporary","romance"],
+      author : `璟梧`,
+      name : "There's Always a Teacher Calling Parents",
+      wordCount : 1858999040,
+      complete : true
     },
     {
-      cat : "original-GL-contemporary-romance",
-      author : "璟梧",
-      bookName : "总有老师要请家长",
-      detailUrl : "3975255",
-      wordCount : "1858999040",
-      status : "515211"
+      tags : ["original","GL","historical","mystery"],
+      author : `君sola`,
+      name : "Tan Xu Ling",
+      wordCount : 2417895936,
+      complete : true
     },
     {
-      cat : "original-GL-contemporary-romance",
-      author : "闵然",
-      bookName : "余情可待[重生]",
-      detailUrl : "2921413",
-      wordCount : "1982098048",
-      status : "525321"
+      tags : ["original","GL","contemporary","romance"],
+      author : `素西`,
+      name : "The Whole World is Waiting for You to Fall in Love",
+      wordCount : 1875406336,
+      complete : true
     },
     {
-      cat : "original-GL-contemporary-romance",
-      author : "一盏夜灯",
-      bookName : "桃李不言gl",
-      detailUrl : "3110061",
-      wordCount : "3296871168",
-      status : "507787"
+      tags : ["original","GL","historical","romance"],
+      author : `请君莫笑`,
+      name : "Female General and Eldest Princess",
+      wordCount : 1563817216,
+      complete : true
     },
     {
-      cat : "original-GL-historical-mystery",
-      author : "君sola",
-      bookName : "探虚陵",
-      detailUrl : "724767",
-      wordCount : "2417895936",
-      status : "1077380"
+      tags : ["original","GL","contemporary","romance"],
+      author : `妲婴`,
+      name : "She Belongs to Me",
+      wordCount : 1383089280,
+      complete : true
     },
     {
-      cat : "original-GL-contemporary-romance",
-      author : "鸽子不会咕咕咕",
-      bookName : "白月光Omega总想拥有我",
-      detailUrl : "5896087",
-      wordCount : "2160391424",
-      status : "613573"
+      tags : ["original","GL","contemporary","romance"],
+      author : `廿廿呀`,
+      name : "Jewel Merchant and Miss Diamond",
+      wordCount : 1919385984,
+      complete : true
     },
     {
-      cat : "original-GL-contemporary-romance",
-      author : "素西",
-      bookName : "全世界都在等你心动",
-      detailUrl : "4273357",
-      wordCount : "1875406336",
-      status : "642415"
+      tags : ["original","GL","contemporary","romance"],
+      author : `叶涩`,
+      name : "Universe's Number One Sour God",
+      wordCount : 1331158272,
+      complete : true
     },
     {
-      cat : "original-GL-historical-romance",
-      author : "请君莫笑",
-      bookName : "女将军和长公主",
-      detailUrl : "3147310",
-      wordCount : "1563817216",
-      status : "585577"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "妲婴",
-      bookName : "她属于我",
-      detailUrl : "3531308",
-      wordCount : "1383089280",
-      status : "196647"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "闵然",
-      bookName : "怦然为你",
-      detailUrl : "3452360",
-      wordCount : "1556496000",
-      status : "584117"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "清汤涮香菜",
-      bookName : "入迷",
-      detailUrl : "4432467",
-      wordCount : "1216079104",
-      status : "355117"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "廿廿呀",
-      bookName : "宝石商人和钻石小姐",
-      detailUrl : "4482691",
-      wordCount : "1919385984",
-      status : "746160"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "叶涩",
-      bookName : "宇宙第一醋神",
-      detailUrl : "3827400",
-      wordCount : "1331158272",
-      status : "534175"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "清汤涮香菜",
-      bookName : "你我相爱，为民除害",
-      detailUrl : "3701673",
-      wordCount : "1091720064",
-      status : "298525"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "鱼霜",
-      bookName : "难缠",
-      detailUrl : "5870745",
-      wordCount : "2296058112",
-      status : "428297"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "一盏夜灯",
-      bookName : "春夏秋冬",
-      detailUrl : "3560429",
-      wordCount : "2212576256",
-      status : "336013"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "鱼霜",
-      bookName : "合久不分",
-      detailUrl : "4131915",
-      wordCount : "2978996224",
-      status : "776035"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "闵然",
-      bookName : "天作不合",
-      detailUrl : "4910099",
-      wordCount : "969833088",
-      status : "208411"
-    },
-    {
-      cat : "original-GL-contemporary-romance",
-      author : "热到昏厥",
-      bookName : "她是主角",
-      detailUrl : "4254762",
-      wordCount : "1803352704",
-      status : "760345"
+      tags : ["original","GL","contemporary","romance"],
+      author : `热到昏厥`,
+      name : "She is the Main Character",
+      wordCount : 1803352704,
+      complete : true
     }
 ]
+function returnItems(){
+  tangerineNovels.forEach(item => console.log(item.name)); //item is var within this scope and => directs what should be done to it
+  tangerineNovels.forEach(item => console.log(item.tags));
+}
+returnItems();
